f28c65e75dafab2aa0022b015651ef13e7f74919
Sebastian <sebatapiareb@gmail.com>
2021-06-11 22:02:04 -0400

    Primer Commit Inutila2

---
+++ flutter_entrega_2/lib/pages/body.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class body extends StatefulWidget {
+  const body({Key key}) : super(key: key);
+
+  @override
+  _bodyState createState() => _bodyState();
+}
+
+class _bodyState extends State<body> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          children: [
+            SizedBox(height: 200),
+            Text('Patente'),
+            TextField(),
+            SizedBox(height: 100),
+            Text('Marca'),
+            SizedBox(height: 100),
+            Text('Precios'),
+          ],
+        ),
+      ),
+    );
+  }
+}
